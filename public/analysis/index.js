b14fb4534919c486e60daa7b2127a30f7d8205d6
maquan <maquan@meituan.com>
2015-07-02 17:00:16 +0800

    format code

--- public/analysis/index.js
+++ public/analysis/index.js
@@ -1,24 +1,24 @@
-$(function() {
+$(function () {
 	var analysis = {};
 	analysis = {
-		initialize: function() {
+		initialize: function () {
 			var self = this;
 			self.analysisData();
 			this.bindUI();
 		},
-		bindUI: function() {
+		bindUI: function () {
 			var self = this;
-			$("#file_upload").click(function() {
+			$("#file_upload").click(function () {
 				self.uploadFile();
 			});
-			$("#J_Files").click(function() {
+			$("#J_Files").click(function () {
 				$("#spanMessage").html("选择文件，并上传");
 			});
-			$("body").delegate("#J_analysis","click",function(){
+			$("body").delegate("#J_analysis", "click", function () {
 				location.href = './handle';
 			})
 		},
-		uploadFile: function() {
+		uploadFile: function () {
 
 			var formData = new FormData($("#frmUploadFile")[0]);
 			$.ajax({
@@ -29,7 +29,7 @@ $(function() {
 				cache: false,
 				contentType: false,
 				processData: false,
-				success: function(data) {
+				success: function (data) {
 					if (200 === data.code) {
 						var span = $("#spanMessage")
 						span.html("上传成功");
@@ -38,48 +38,49 @@ $(function() {
 						$("#spanMessage").html("上传失败");
 					}
 				},
-				error: function() {
+				error: function () {
 					$("#spanMessage").html("与服务器通信发生错误");
 				}
 			});
 		},
-		analysisData: function() {
+		analysisData: function () {
 			var self = this;
 			var os = {};
 			var deviceType = {};
 			var deviceName = {};
 			var browserName = {};
 			if (!window.UADATA) return;
-			$("#J_num").text(window.UADATA.length -1);
-			$.each(UADATA, function(index, items) {
-					var item = detector.parse(items[0]);
-					//计算os的平台及个数
-					var ons = item.os.name;
-					if (ons in os) {
-						os[ons]++;
-					} else {
-						os[ons] = 1;
-					}
-					//计算设备厂家
-					var dt = item.device.name;
-					if (dt in deviceType) {
-						deviceType[dt]++;
-					} else {
-						deviceType[dt] = 1;
-					}
-					//计算设备型号
-					var dn = item.device.name + (item.device.fullVersion == -1 ? '' : ' ' + item.device.fullVersion);
-					if (dn in deviceName) {
-						deviceName[dn]++;
-					} else {
-						deviceName[dn] = 1;
-					}
-					var bn = item.browser.name;
-					if (bn in browserName) {
-						browserName[bn]++;
-					} else {
-						browserName[bn] = 1;
-					}
+			$("#J_num").text(window.UADATA.length - 1);
+			$.each(UADATA, function (index, items) {
+				var item = detector.parse(items[0]);
+				//计算os的平台及个数
+				var ons = item.os.name;
+				if (ons in os) {
+					os[ons]++;
+				} else {
+					os[ons] = 1;
+				}
+				//计算设备厂家
+				var dt = item.device.name;
+				if (dt in deviceType) {
+					deviceType[dt]++;
+				} else {
+					deviceType[dt] = 1;
+				}
+				//计算设备型号
+				var dn = item.device.name + (item.device.fullVersion == -1 ? '' : ' ' + item.device.fullVersion);
+				if (dn in deviceName) {
+					deviceName[dn]++;
+				} else {
+					deviceName[dn] = 1;
+				}
+				//浏览器分布
+				var bn = item.browser.name;
+				if (bn in browserName) {
+					browserName[bn]++;
+				} else {
+					browserName[bn] = 1;
+				}
 			})
 
 			self.chart(self.objToArray(os), 'os的平台及个数')
@@ -88,7 +89,7 @@ $(function() {
 			self.chart(self.objToArray(browserName), '浏览器分布')
 
 		},
-		objToArray: function(obj) {
+		objToArray: function (obj) {
 			var tempArr = [];
 			for (var i in obj) {
 				var ta = [];
@@ -98,7 +99,7 @@ $(function() {
 			}
 			return tempArr;
 		},
-		chart: function(data, title) {
+		chart: function (data, title) {
 			var ts = +new Date();
 			var ele = document.createElement('div');
 			ele.setAttribute("id", "chart_" + ts);
